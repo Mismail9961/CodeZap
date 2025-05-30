ef75f94083a9bf2a604f959bf871bcf5efe75d63
MUHAMMAD ISMAIL <150179291+Mismail9961@users.noreply.github.com>
2025-05-30 23:40:06 +0500

    Database,Storage and multiple settings

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+import { Client, Account , Avatars , Storage , Databases } from 'node-appwrite';
+import env from '@/app/env';
+const client = new Client()
+    .setEndpoint(env.appwrite.endpoint)
+    .setProject(env.appwrite.projectId)
+    .setKey(env.appwrite.apikey);
+
+
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client)
+
+
+export { client, account ,databases , avatars ,storage };
